7670ad4a3c03f0b64a53ee6b7bac2c6a014b43db
MarkSill <marksillweb@gmail.com>
2015-11-05 18:04:28 -0800

    Basic functions

--- maze.js
+++ maze.js
@@ -1,8 +1,29 @@
-var maze = {};
+var maze = {
+	data: {
+		name: "Maze",
+		author: "Person",
+		px: 0,
+		py: 0,
+		tiles: []
+	}
+};
 (function() {
 	"use strict";
 
+	maze.create = function(data) {
+		maze.data = JSON.parse(data);
+		maze.update();
+	};
+
+	maze.getData = function() {
+		return JSON.stringify(maze.data);
+	};
+
 	maze.random = function(size) {
+
+	};
+
+	maze.update = function() {
 
 	};
 })();
